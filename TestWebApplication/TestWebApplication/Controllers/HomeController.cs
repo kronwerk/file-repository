afdd0506852802ea73c2cdc2fca984d242cc7e5a
kronwerk <tetragon@mail.ru>
2015-10-09 10:41:09 +0300

    attempt to make FilePage work

--- TestWebApplication/TestWebApplication/Controllers/HomeController.cs
+++ TestWebApplication/TestWebApplication/Controllers/HomeController.cs
@@ -13,8 +13,27 @@ namespace TestWebApplication.Controllers
             return View();
         }
 
-        public ActionResult FilePage()
+        public ActionResult FilePage(int id = 0)
         {
+            string type = "text";
+            int max = 2; int min = 0; 
+            if (id.CompareTo(min) < 0)
+                id = 0;
+            else if (id.CompareTo(max) > 0)
+                id = max;
+            if (id.Equals(0))
+            {
+                ViewBag.Content = "Ни одной сказки, песни, басни он не знал и наполовину. Куда?! Котам нельзя, с котами нельзя!!!";
+            } else if (id.Equals(1))
+            {
+                type = "img";
+            } else if (id.Equals(2))
+            {
+                type = "audio";
+            };
+            ViewBag.ID = id;
+            ViewBag.Name = String.Format("{0} {1}", type, id);
+            ViewBag.Type = type;
             return View();
         }
 
