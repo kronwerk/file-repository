95f9c240891fc071f2095619e509e8bde7ce758f
Alex Kochetkov <206860@niuitmo.ru>
2015-10-17 14:45:39 +0300

    починил логин и регистрацию

--- TestWebApplication/TestWebApplication/Controllers/AccountController.cs
+++ TestWebApplication/TestWebApplication/Controllers/AccountController.cs
@@ -66,16 +66,16 @@ namespace TestWebApplication.Controllers
         [HttpPost]
         [AllowAnonymous]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Login(LoginViewModel model, string returnUrl)
+        public async Task<ActionResult> Login(SignInUp model, string returnUrl)
         {
             if (!ModelState.IsValid)
             {
-                return View(model);
+                return View(model.LoginViewModel);
             }
 
             // This doesn't count login failures towards account lockout
             // To enable password failures to trigger account lockout, change to shouldLockout: true
-            var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
+            var result = await SignInManager.PasswordSignInAsync(model.LoginViewModel.Email, model.LoginViewModel.Password, model.LoginViewModel.RememberMe, shouldLockout: false);
             switch (result)
             {
                 case SignInStatus.Success:
@@ -83,11 +83,11 @@ namespace TestWebApplication.Controllers
                 case SignInStatus.LockedOut:
                     return View("Lockout");
                 case SignInStatus.RequiresVerification:
-                    return RedirectToAction("SendCode", new { ReturnUrl = returnUrl, RememberMe = model.RememberMe });
+                    return RedirectToAction("SendCode", new { ReturnUrl = returnUrl, RememberMe = model.LoginViewModel.RememberMe });
                 case SignInStatus.Failure:
                 default:
                     ModelState.AddModelError("", "Invalid login attempt.");
-                    return View(model);
+                    return View(model.LoginViewModel);
             }
         }
 
@@ -147,12 +147,12 @@ namespace TestWebApplication.Controllers
         [HttpPost]
         [AllowAnonymous]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Register(RegisterViewModel model)
+        public async Task<ActionResult> Register(SignInUp model)
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
-                var result = await UserManager.CreateAsync(user, model.Password);
+                var user = new ApplicationUser { UserName = model.RegisterViewModel.Email, Email = model.RegisterViewModel.Email };
+                var result = await UserManager.CreateAsync(user, model.RegisterViewModel.Password);
                 if (result.Succeeded)
                 {
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
@@ -169,7 +169,7 @@ namespace TestWebApplication.Controllers
             }
 
             // If we got this far, something failed, redisplay form
-            return View(model);
+            return View(model.RegisterViewModel);
         }
 
         //
