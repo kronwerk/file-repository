fc6e776959034169aa3b81f80458aa1528559c12
Alex Kochetkov <206860@niuitmo.ru>
2015-10-20 19:39:13 +0300

    изменил форму регистрации

--- TestWebApplication/TestWebApplication/Models/AccountViewModels.cs
+++ TestWebApplication/TestWebApplication/Models/AccountViewModels.cs
@@ -79,6 +79,18 @@ namespace TestWebApplication.Models
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 2)]
+        [DataType(DataType.Password)]
+        [Display(Name = "First name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 2)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Last name")]
+        public string LastName { get; set; }
     }
 
     public class ResetPasswordViewModel
