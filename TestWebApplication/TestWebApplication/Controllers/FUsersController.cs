77276bfffd853fdc2185594026344d271b4ddf88
Alex Kochetkov <206860@niuitmo.ru>
2015-11-17 13:26:46 +0300

    added controller and views for FUser. it will replace accountController

---
+++ TestWebApplication/TestWebApplication/Controllers/FUsersController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TestWebApplication.Models;
+
+namespace TestWebApplication.Controllers
+{
+    public class FUsersController : Controller
+    {
+        private UserDBContext db = new UserDBContext();
+
+        // GET: FUsers
+        public ActionResult Index()
+        {
+            return View(db.FUsers.ToList());
+        }
+
+        // GET: FUsers/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            FUser fUser = db.FUsers.Find(id);
+            if (fUser == null)
+            {
+                return HttpNotFound();
+            }
+            return View(fUser);
+        }
+
+        // GET: FUsers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: FUsers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "FUserID,FirstName,LastName,age,gender,city")] FUser fUser)
+        {
+            if (ModelState.IsValid)
+            {
+                db.FUsers.Add(fUser);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(fUser);
+        }
+
+        // GET: FUsers/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            FUser fUser = db.FUsers.Find(id);
+            if (fUser == null)
+            {
+                return HttpNotFound();
+            }
+            return View(fUser);
+        }
+
+        // POST: FUsers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "FUserID,FirstName,LastName,age,gender,city")] FUser fUser)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(fUser).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(fUser);
+        }
+
+        // GET: FUsers/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            FUser fUser = db.FUsers.Find(id);
+            if (fUser == null)
+            {
+                return HttpNotFound();
+            }
+            return View(fUser);
+        }
+
+        // POST: FUsers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            FUser fUser = db.FUsers.Find(id);
+            db.FUsers.Remove(fUser);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
