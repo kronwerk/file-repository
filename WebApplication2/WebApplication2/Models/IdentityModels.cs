a89a5a2e7eb94a74ce8f763e88bb08b1abb0ea2d
Alex Kochetkov <206860@niuitmo.ru>
2016-01-09 22:20:09 +0300

    newOne
    полностью новый проект. новый дизайн, есть функции фхода и регистрации,
    смена пароля.

---
+++ WebApplication2/WebApplication2/Models/IdentityModels.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+
+namespace WebApplication2.Models
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string EMail { get; set; }
+        public string LastName { get; set; }
+        public string Age { get; set; }
+        public string City { get; set; }
+        public int NumberOfRepos = 0;
+    }
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection")
+        {
+        }
+    }
+}
